2a88d69670eb183ec5d4c8364691a4a00e47cc5b
kriostt <129346100+kriostt@users.noreply.github.com>
2024-03-09 23:47:27 -0700

    Implemented bootstrap for the navbar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,58 +1,66 @@
 // import necessary modules and external files
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { navItems } from "./NavItems";
-import ProductCategoryDropdown from "./ProductCategoryDropdown";
 import "./Navbar.css";
 
-function Navbar() {
-  // initialize dropdown state
-  const [dropdown, setDropdown] = useState(false);
-
+const Navbar = () => {
   // JSX for navbar component
   return (
     <>
-      <nav className="navbar">
-        {/* logo takes user back to home page when clicked */}
-        <Link to="/" className="navbar-logo">
-          RDP BAZAAR
-        </Link>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          {/* logo takes user back to home page when clicked */}
+          <Link to="/" className="navbar-brand">
+            RDP BAZAAR
+          </Link>
 
-        {/* unordered list for navigation items */}
-        <ul className="nav-items">
+          {/* button collapse behaviour of navbar on smaller screens */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-          {/* map through each item in navItems array to designate navbar items */}
-          {navItems.map((item) => {
-            // if navigation item is shop, render the dropdown for it
-            if (item.title === "Shop") {
-              return (
-                <li
-                  key={item.id}
-                  className={item.cName}
-                  onMouseEnter={() => setDropdown(true)}
-                  onMouseLeave={() => setDropdown(false)}
-                >
-                  {/* link for when shop is clicked */}
-                  <Link to={item.path}>{item.title}</Link>
-
-                  {/* render product category dropdown component when dropdown is true */}
-                  {dropdown && <ProductCategoryDropdown />}
-                </li>
-              );
-            }
-
-            // display other navigation links
-            return (
-              <li key={item.id} className={item.cName}>
-                <Link to={item.path}>{item.title}</Link>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            {/* unordered list for navigation items */}
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/" className="nav-link" aria-current="page">
+                  Home
+                </Link>
               </li>
-            );
-          })}
-        </ul>
+              <li className="nav-item dropdown">
+                <Link
+                  to="/shop"
+                  className="nav-link dropdown-toggle"
+                  role="button"
+                  aria-expanded="false"
+                >
+                  Shop
+                </Link>
+                <ul className="dropdown-menu">
+                  <li><Link to="/appliances" className="dropdown-item">Appliances</Link></li>
+                  <li><Link to="/clothing" className="dropdown-item">Clothing</Link></li>
+                  <li><Link to="/electronics" className="dropdown-item">Electronics</Link></li>
+                  <li><Link to="/Furniture" className="dropdown-item">Furniture</Link></li>
+                  <li><Link to="/Miscellaneous" className="dropdown-item">Miscellaneous</Link></li>
+                  <li><Link to="/Textbooks" className="dropdown-item">Textbooks</Link></li>
+                  <li><Link to="/Vehicles" className="dropdown-item">Vehicles</Link></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </>
   );
-}
+};
 
 // export navbar component
 export default Navbar;
