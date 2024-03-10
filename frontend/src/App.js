b62948e7a6261ef5214549a5002edbae97f895cc
kriostt <129346100+kriostt@users.noreply.github.com>
2024-03-09 20:12:31 -0700

    Edit App.js to include navbar and dropdown components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,44 @@
+// import necessary modules and external files
 import React from "react";
-import SearchBar from "./components/SearchBar"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SearchBar from "./components/SearchBar";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+
+import Appliances from "./pages/Appliances";
+import Clothing from "./pages/Clothing";
+import Electronics from "./pages/Electronics";
+import Furniture from "./pages/Furniture";
+import Miscellaneous from "./pages/Miscellaneous";
+import Textbooks from "./pages/Textbooks";
+import Vehicles from "./pages/Vehicles";
 
 function App() {
+  // JSX for app component
   return (
-    <div className="App">
+    <Router>
+      {/* display navigation bar and search bar components */}
+      <Navbar />
       <SearchBar />
-    </div>
+
+      {/* define routes for different pages */}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+
+        <Route path="/appliances" element={<Appliances />}></Route>
+        <Route path="/clothing" element={<Clothing />}></Route>
+        <Route path="/electronics" element={<Electronics />}></Route>
+        <Route path="/furniture" element={<Furniture />}></Route>
+        <Route path="/miscellaneous" element={<Miscellaneous />}></Route>
+        <Route path="/textbooks" element={<Textbooks />}></Route>
+        <Route path="/vehicles" element={<Vehicles />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
+// export app component
 export default App;
