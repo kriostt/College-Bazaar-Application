62e6f84535ec3b95dfd29b8a171b8b09b5d63892
kriostt <129346100+kriostt@users.noreply.github.com>
2024-03-12 17:33:10 -0600

    Revert "Merge branch 'dev' into featureBackend-SearchAndFilter"
    This reverts commit e46e65f105fa6beb4200a21007c7bcefba58eff2, reversing
    changes made to 35319523940955fd1d6d0fb7166535217e12c483.
    Revert push

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,10 +3,9 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // Indicates that this is the main Spring Boot application class
+@SpringBootApplication
 public class BackendApplication {
 	public static void main(String[] args) {
-		// Start the Spring Boot application
 		SpringApplication.run(BackendApplication.class, args);
 	}
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
-import { Routes, Route } from "react-router-dom"; 
-import EditProfile from "./components/ProfileManagement/EditProfile";
-import ChangePassword from "./components/ProfileManagement/ChangePassword";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<EditProfile />} />
-      <Route path="/change-password" element={<ChangePassword />} />
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
